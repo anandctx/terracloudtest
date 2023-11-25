df5721433fd365ef2799ac5ce0a02a5ce9baf3b6
Anand Rao <anand.rao@wpp.com>
2023-11-25 07:50:05 +0530

    a

--- rg.tf
+++ rg.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "example" {
   name     = "tc-cloud-test-rg"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "example2" {
+  name     = "tc-cloud-test-rg"
+  location = "West Europe"
+}
